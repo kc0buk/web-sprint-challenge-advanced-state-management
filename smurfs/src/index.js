9b95108ac73e93c694e3d3d85d8715ce24d986ec
Jeremiah Trnka <jeremiah@jeremiahs-mbp.lan>
2020-08-14 09:09:40 -0700

    Updated readme and index.js with app plan.

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,24 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
+// Global state will be store in redux store, initial state will live in smurfReducer
+/* State will have the following shape:
+    smurfs: [ {...}, {...}, {...} ], // Holds the array of smurf objects
+        //each smurf object has the following shape
+        {
+            "name": "Brainey",
+            "age": 200,
+            "height": "5cm",
+            "id": 0
+        }
+    isLoading: false, // Handles loading screen or not
+    error: '', // Holds error message from API
+*/
+
+// Smurf state will be needed to render a card for each result
+// isLoading and error states will be needed for the homepage to determine global app state, (e.g. loading, display data, error)
+
+// Smurf get request will live in hompage component
+// addSmurf post request will live in form component
+
 ReactDOM.render(<App />, document.getElementById("root"));
